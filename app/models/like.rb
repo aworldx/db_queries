7fca77ce17ebd7afb4aa5425aced0d9e98cd889d
Амир Хасанов <aworldx@gmail.com>
2018-08-19 14:47:52 +0300

    edit readme

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user, counter_cache: true
   belongs_to :post, counter_cache: true
+
+  def self.add_like(post_id)
+    Like.transaction do
+      likes = Like.where(post_: post_id).lock(true)
+      
+      sleep(1000000)
+    end
+  end
 end
